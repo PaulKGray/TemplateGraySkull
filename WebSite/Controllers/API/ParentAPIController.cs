0ee2983c3a72e57767700438550d48d7a6c69230
Paul <Paul.kinross.gray@gmail.com>
2013-10-22 21:09:06 +0100

    moving api into project

---
+++ WebSite/Controllers/API/ParentAPIController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Template.Helpers.Converters;
+using Template.Models;
+using Template.Services.Interfaces;
+
+namespace Template.Controllers
+{
+    public class ParentAPIController : ApiController
+    {
+        
+        private IParentItemService _ParentItemService;
+
+
+        public ParentAPIController(IParentItemService parentItemService)
+        {
+            _ParentItemService = parentItemService;
+        }
+
+ 
+        public IList<ParentItemModel> GetAllParents()
+        {
+            var converter = new ParentItemConverter();  
+            var parentItems = converter.convertParentItemDomainObject(_ParentItemService.GetAllParentItem());
+            return parentItems;
+        }
+
+    }
+}
---
+++ WebSite/Helpers/Converters/ParentItemConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Template.Domain;
+using Template.Models;
+
+namespace Template.Helpers.Converters
+{
+    public class ParentItemConverter
+    {
+        /// <summary>
+        /// Convert a list of parent items to parent item models
+        /// </summary>
+        /// <param name="parentItems">the list of parent items to convert</param>
+        /// <returns>The list of parent item models</returns>
+        public IList<ParentItemModel> convertParentItemDomainObject(IList<ParentItem> parentItems) {
+            var parentItemModels = new List<ParentItemModel>();
+
+            foreach (var parentItem in parentItems)
+            {
+                parentItemModels.Add(convertParentItemDomainObject(parentItem));
+            }
+
+            return parentItemModels;
+        }
+
+        /// <summary>
+        /// Convert a parent item to a patent item model
+        /// </summary>
+        /// <param name="parentItem">The parent item to convert</param>
+        /// <returns>The parent item model</returns>
+        public ParentItemModel convertParentItemDomainObject(ParentItem parentItem)
+        {
+
+            var parentItemModel = new ParentItemModel();
+            parentItemModel.ParentItemid = parentItem.ParentItemid;
+            parentItemModel.Name = parentItem.Name;
+            parentItemModel.Description = parentItem.Description;
+
+            foreach (var childItem in parentItem.ChildItems)
+            {
+
+                var newChildItem = new ChildItemModel();
+                newChildItem.ChildItemId = childItem.ChildItemId;
+                newChildItem.Name = childItem.Name;
+
+                parentItemModel.ChildItems.Add(newChildItem);
+
+            }
+
+            return parentItemModel;
+
+        }
+    }
+}
